489c8513b61935830ee15973f1ba598173cc5ffa
Dmitry Harnitski <dmitry.harnitski@careerbuilder.com>
2019-05-01 15:54:44 -0400

    add comment for import

--- app/candidates/store/candidate-list.effects.ts
+++ app/candidates/store/candidate-list.effects.ts
@@ -2,8 +2,11 @@ import { Observable, of } from "rxjs";
 import { Action, Store } from "@ngrx/store";
 import { Effect, Actions, ofType } from "@ngrx/effects";
 import { catchError, mergeMap, switchMap, tap, withLatestFrom, mapTo } from "rxjs/operators";
-
 import { FETCH_CANDIDATES_WITH_FILTER, FetchCandidatesWithFilter, TEST } from "./candidate-list.actions";
+
+// import { FETCH_CANDIDATES_WITH_FILTER, FetchCandidatesWithFilter, TEST } from "~/app/candidates/store/candidate-list.actions";
+// /app/candidates/store/candidate-list.actions
+
 import * as candidateState from './candidates.state';
 
 export class CandidateListEffects {
